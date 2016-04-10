e7d56a58a2790bf93115e902642f9dd43c7d1cf3
Eelke Hermens <eelkehermens@gmail.com>
2016-04-10 20:57:22 +0200

    no demo in path

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'demo'),
     filename: 'bundle.js',
-    publicPath: '/demo/'
+    publicPath: '/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
